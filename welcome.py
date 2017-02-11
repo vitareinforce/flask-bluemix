16c843b922c740b5046475d2284aa8e6e20cc1d2
Vitradisa Pratama <vita.reinforce@gmail.com>
2017-02-11 23:42:21 +0700

    edit import

--- welcome.py
+++ welcome.py
@@ -15,7 +15,7 @@
 import os
 from flask import Flask, jsonify, Response, json
 from flask_cors import CORS, cross_origin
-from flight_data import flight_data
+from flightdata import flight_data
 
 app = Flask(__name__)
 app.register_blueprint(flight_data)
